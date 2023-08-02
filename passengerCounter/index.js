47ba23ba24a14910efbd67ba76878a43d6824923
aditya <adityadewhy@gmail.com>
2023-08-02 16:41:56 +0530

    added save-btn

--- passengerCounter/index.js
+++ passengerCounter/index.js
@@ -1,6 +1,9 @@
 let count = 0;
 function increment() {
     count += 1;
-    console.log(count);
     document.getElementById("count-el").innerText = count;
+}
+
+function save(){
+    console.log(count);
 }
